b055bcb072468070b71c020cd4077cd9ff51f05e
owlund <36522255+owlund@users.noreply.github.com>
2018-02-16 00:31:47 +0100

    My first Arduino app

---
+++ ElapsedTime_feb15a.ino
@@ -0,0 +1,22 @@
+int seconds;
+int minutes;
+void setup() {
+  // put your setup code here, to run once:
+  seconds = 0;
+  minutes = 0; 
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(minutes);
+  Serial.print(":");
+  Serial.println(seconds);
+  delay(1000);
+  seconds++;
+  if(seconds == 60)
+  {minutes++;
+  seconds = 0;
+  }
+}
